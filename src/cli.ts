e349b41036964addc7e449faa193cc32b4135135
Jorge Schrauwen <sjorge@acheron.be>
2024-02-03 16:42:47 +0100

    feat: add configuration command

---
+++ src/cli.ts
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+import { program } from '@commander-js/extra-typings';
+import { configureAction } from './configure';
+//import { webhookAction } from './webhook';
+
+program
+    .name("anidb-watched-sync")
+    .version("1.0.0")
+    .description("Webhook for syncronizing plex and jellyfin watch status for anidb tagged episodes.");
+
+program
+    .command('configure')
+    .description('update configuration file')
+    .option('--webhook-bind <ip>', 'optional IP where webhook binds on (default to localhost)')
+    .option('--webhook-port <port>', 'optional port where webhook binds on (default to 4091)')
+    .option('--jellyfin-url <url>', 'jellyfin server URL')
+    .option('--jellyfin-api-key <api_key>', 'jellyfin API key')
+    .option('--jellyfin-ca-file <ca_file>', 'optional CA file for jellyfin')
+    .option('--jellyfin-library <library>', 'jellyfin anime library name')
+    .option('--jellyfin-user <username>', 'jellyfin username')
+    .option('--plex-url <url>', 'plex server URL')
+    .option('--plex-token <token>', 'plex token')
+    .option('--plex-library <library>', 'plex anime library name')
+    .option('--plex-user <username>', 'plex username')
+    .action(configureAction);
+
+/*
+program
+    .command('webhook')
+    .description('Start the webhook server')
+    .action(webhookAction);
+*/
+
+program.parse(process.argv);
+
+// vim: tabstop=4 shiftwidth=4 softtabstop=0 smarttab expandtab
---
+++ src/configure.ts
@@ -0,0 +1,99 @@
+import { OptionValues } from '@commander-js/extra-typings';
+import os from 'node:os';
+import fs from 'node:fs';
+import path from 'node:path';
+import process from 'node:process';
+import toml from '@iarna/toml';
+import { deepmerge } from 'deepmerge-ts';
+
+
+export type Config = {
+    webhook: {
+        bind: string;
+        port: number;
+    },
+    jellyfin: {
+        url?: string;
+        apiKey?: string;
+        caFile?: string;
+        library?: string;
+        user?: string;
+    };
+    plex: {
+        url?: string;
+        token?: string;
+        library?: string;
+        user?: string;
+    };
+};
+
+const configFile: string = path.join(os.homedir(), '.config', 'pjaws', 'config.toml');
+
+export function readConfig(): Config {
+    let config: Config = {
+        webhook: {
+            bind: 'localhost',
+            port: 4091,
+        },
+        jellyfin: {},
+        plex: {},
+    };
+
+    if(fs.existsSync(configFile) && fs.statSync(configFile).isFile()) {
+        const configToml = toml.parse(fs.readFileSync(configFile, 'utf8')) as Config;
+        config = deepmerge(config, configToml);
+    }
+
+    return config;
+}
+
+function writeConfig(config: Config): boolean {
+    try {
+        const configFilePath: string = path.dirname(configFile);
+        if(!fs.existsSync(configFilePath)) {
+            fs.mkdirSync(configFilePath, { recursive: true, mode: 0o750 });
+        }
+        fs.writeFileSync(configFile, toml.stringify(config), { encoding: 'utf8' });
+        fs.chmodSync(configFile, 0o600);
+    } catch (error) {
+        return false;
+    }
+
+    return true;
+}
+
+export function validateConfig(config: Config): boolean {
+    if (config.jellyfin.url == undefined) return false;
+    if (config.jellyfin.apiKey == undefined) return false;
+    if (config.jellyfin.user == undefined) return false;
+    if (config.jellyfin.library == undefined) return false;
+    if (config.plex.url == undefined) return false;
+    if (config.plex.token == undefined) return false;
+    if (config.plex.user == undefined) return false;
+    if (config.plex.library == undefined) return false;
+    return true;
+}
+
+export async function configureAction(opts: OptionValues): Promise<void> {
+    const config: Config = readConfig();
+    if (opts.webhookBind) config.webhook.bind = `${opts.webhookBind}`;
+    if (opts.webhookPort) config.webhook.port = parseInt(`${opts.webhookPort}`);
+    if (opts.jellyfinUrl) config.jellyfin.url = `${opts.jellyfinUrl}`;
+    if (opts.jellyfinApiKey) config.jellyfin.apiKey = `${opts.jellyfinApiKey}`;
+    if (opts.jellyfinCaFile) config.jellyfin.caFile = `${opts.jellyfinCaFile}`;
+    if (opts.jellyfinLibrary) config.jellyfin.library = `${opts.jellyfinLibrary}`;
+    if (opts.jellyfinUser) config.jellyfin.user = `${opts.jellyfinUser}`;
+    if (opts.plexUrl) config.plex.url = `${opts.plexUrl}`;
+    if (opts.plexToken) config.plex.token = `${opts.plexToken}`;
+    if (opts.plexLibrary) config.plex.library = `${opts.plexLibrary}`;
+    if (opts.plexUser) config.plex.user = `${opts.plexUser}`;
+    if(!writeConfig(config)) {
+        console.error(`Failed to update ${configFile}!`);
+        process.exitCode = 1;
+    }
+    if(!validateConfig(config)) {
+        console.warn(`Configuration is not valid! Please specify at least plex url, token, user, and library and jellyfin url, api-key, user, and library.`);
+    }
+}
+
+// vim: tabstop=4 shiftwidth=4 softtabstop=0 smarttab expandtab
