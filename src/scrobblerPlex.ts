ed098c9c18559039a94589ac6e687ed774c81e8e
Jorge Schrauwen <sjorge@acheron.be>
2024-05-25 19:26:44 +0200

    feat: support syncing playback progess from jellyfin -> plex
    We support syncing playback progess from jellyfin to plex, to not hammer the API to badly we only sync roughly ever 15 seconds.
    Even so, we still hammer the API quite a bit more than I'd like. But this seems usable.

--- src/scrobblerPlex.ts
+++ src/scrobblerPlex.ts
@@ -41,6 +41,58 @@ export class ScrobblerPlex {
         });
     }
 
+    public async progress(aid: number, time: number, episode: number, season: number = 1): Promise<ScrobbleResult> {
+        if (this.api == undefined) {
+            return {
+                success: false,
+                log_lvl: "error",
+                log_msg: "not initialized!",
+            } as ScrobbleResult;
+        }
+
+        // GUID format com.plexapp.agents.hama://anidb-<aid>/<season>/<episode>?lang=<metadata_lang>
+        const seriesGuid = `com.plexapp.agents.hama://anidb-${aid}?lang=`;
+        const seasonGuid = `com.plexapp.agents.hama://anidb-${aid}/${season}?lang=`;
+        const episodeGuid = `com.plexapp.agents.hama://anidb-${aid}/${season}/${episode}?lang=`;
+
+        let foundEpisode = false;
+        const libraries = await this.api.find("/library/sections", {agent: "com.plexapp.agents.hama"});
+        for (const library of libraries) {
+            if (!this.config.plex.library.includes(library.title)) continue;
+            for (const series of await this.api.find(`/library/sections/${library.key}/all`, {type: "show"})) {
+                if (!series.guid.startsWith(seriesGuid)) continue;
+                for (const season of await this.api.find(`/library/metadata/${series.ratingKey}/children`, {type: "season"})) {
+                    if (!season.guid.startsWith(seasonGuid)) continue;
+                    for (const episode of await this.api.find(`/library/metadata/${season.ratingKey}/children`, {type: "episode"})) {
+                        if (!episode.guid.startsWith(episodeGuid)) continue;
+                        foundEpisode = true;
+                        if ((episode.viewCount == undefined) || (episode.viewCount < 1)) {
+                            try {
+                                await this.api.query(`/:/progress?identifier=com.plexapp.plugins.library&key=${episode.ratingKey}&time=${time}`);
+                            } catch (err) {
+                                return {
+                                    success: false,
+                                    log_lvl: "error",
+                                    log_msg: `api returned and unexpected error!`,
+                                } as ScrobbleResult;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return (foundEpisode) ? {
+            success: true,
+            log_lvl: "info",
+            log_msg: "successful.",
+        } as ScrobbleResult : {
+            success: false,
+            log_lvl: "warn",
+            log_msg: `could not find series with anidb id ${aid} on server.`,
+        } as ScrobbleResult;
+    }
+
     public async scrobble(aid: number, episode: number, season: number = 1): Promise<ScrobbleResult> {
         if (this.api == undefined) {
             return {
--- src/webhookJellyfin.ts
+++ src/webhookJellyfin.ts
@@ -16,6 +16,9 @@ type JellyfinPayload = {
     UserId: string;
     SeriesName: string;
     Name: string;
+    PlaybackPositionTicks?: number,
+    RunTimeTicks?: number,
+    IsPaused?: boolean;
 };
 
 export async function webhookJellyfin(config: Config, scrobbler: Scrobblers, req: Request, reqid: string): Promise<Response> {
@@ -34,13 +37,13 @@ export async function webhookJellyfin(config: Config, scrobbler: Scrobblers, req
         const data: JellyfinPayload|undefined = await req.json() as JellyfinPayload;
         if (data == undefined) return new Response("Bad Request", {status: 400, statusText: "Bad Request"});
 
-        log(`[${reqid}] jellyfin: event=${data.NotificationType} completed=${data.PlayedToCompletion} username=${data.NotificationUsername} ...`);
         if ( // NOTE: no (easy?) way to get libraryId or name, assuming webhook is configured correctly
             (data.NotificationType == "PlaybackStop") &&
             (data.PlayedToCompletion == true) &&
             (data.ItemType == "Episode") &&
             (data.NotificationUsername == config.jellyfin.user)
         ) {
+            log(`[${reqid}] jellyfin: event=${data.NotificationType} username=${data.NotificationUsername} completed=${data.PlayedToCompletion} ...`);
             // retrieve required information
             const episodeData: JellyfinEpisodeDetail|undefined = await api.getEpisodeData(data.ItemId, data.UserId);
             if ((episodeData != undefined) && (episodeData.ProviderIds.anidb != undefined)) {
@@ -70,8 +73,41 @@ export async function webhookJellyfin(config: Config, scrobbler: Scrobblers, req
             } else {
                 log(`[${reqid}] jellyfin: ignoring, could not retrieve needed data.`, "warn");
             }
+        } else if (
+            (data.NotificationType == "PlaybackProgress") &&
+            (data.ItemType == "Episode") &&
+            (data.NotificationUsername == config.jellyfin.user) &&
+            ((data.IsPaused !== undefined) && (!data.IsPaused)) &&
+            (scrobbler.plex?.scrobble !== undefined)
+        ) {
+            let progress = 0;
+            let plex_runtime = 0;
+            if (data.PlaybackPositionTicks && data.RunTimeTicks) {
+                progress = (data.PlaybackPositionTicks / data.RunTimeTicks) * 100;
+                progress = Math.round(progress * 100) / 100;
+                plex_runtime = Math.round(data.PlaybackPositionTicks / 10000);
+            }
+            log(`[${reqid}] jellyfin: event=${data.NotificationType} username=${data.NotificationUsername} progress=${progress}% ...`);
+            if ((plex_runtime >= 60001) && (plex_runtime % 15000 < 1001)) {
+                const episodeData: JellyfinEpisodeDetail|undefined = await api.getEpisodeData(data.ItemId, data.UserId);
+                if ((episodeData != undefined) && (episodeData.ProviderIds.anidb != undefined)) {
+                    const anidb_id: number = episodeData.ProviderIds.anidb;
+                    const media_series = (data.SeriesName != undefined) ? data.SeriesName : "Unknown Series";
+                    const media_season = (episodeData.ParentIndexNumber != undefined) ? episodeData.ParentIndexNumber : -1;
+                    const media_episode = (episodeData.IndexNumber != undefined) ? episodeData.IndexNumber : -1;
+                    const media_title = (data.Name != undefined) ? data.Name : "Unknown Title";
+                    log(`[${reqid}] jellyfin: detected as ${anidb_id}: ${media_series} - S${media_season}E${media_episode} - ${media_title}!`);
+
+                    if ((media_season >= 0) && (media_episode >= 0)) {
+                        scrobbler.plex.progress(anidb_id, plex_runtime, media_episode, media_season)
+                            .then((result: ScrobbleResult) => {
+                                log(`[${reqid}] jellyfin: progress send to plex: ${result.log_msg}`, result.log_lvl);
+                            });
+                    }
+                }
+            }
         } else {
-            log(`[${reqid}] jellyfin: ignoring, not interested in this event.`);
+            log(`[${reqid}] jellyfin: event=${data.NotificationType} username=${data.NotificationUsername}, ignoring, not interested in this event.`);
         }
 
         return new Response(`[${reqid}] jellyfin: OK`);
