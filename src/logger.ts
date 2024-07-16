b707ccb364500ee579dde5c6469bccac46de24e2
Jorge Schrauwen <sjorge@acheron.be>
2024-07-16 12:55:12 +0200

    fix: logger should use terminal escapes to blank line

--- src/logger.ts
+++ src/logger.ts
@@ -3,39 +3,38 @@ import process from 'node:process';
 
 export function log(msg: string, type: "error" | "warn" | "step" | "done" | "info" = "info"): void {
     const useColor: boolean = tty.isatty(process.stdout.fd);
-    const blank: number = process.stdout.columns ? (process.stdout.columns) : 0;
     switch(type) {
         case "error":
             if (useColor) {
-                process.stderr.write(`\r${" ".repeat(blank)}\r[\x1b[31m!!\x1b[0m] ${msg}\n`);
+                process.stderr.write(`\x1b[2K\r[\x1b[31m!!\x1b[0m] ${msg}\n`);
             } else {
-                process.stdout.write(`[EE] ${msg}\n`);
+                process.stdout.write(`[!!] ${msg}\n`);
             }
             break;
         case "warn":
             if (useColor) {
-                process.stdout.write(`\r${" ".repeat(blank)}\r[\x1b[33mWW\x1b[0m] ${msg}\n`);
+                process.stdout.write(`\x1b[2K\r[\x1b[33mWW\x1b[0m] ${msg}\n`);
             } else {
                 process.stdout.write(`[WW] ${msg}\n`);
             }
             break;
         case "info":
             if (useColor) {
-                process.stdout.write(`\r${" ".repeat(blank)}\r[\x1b[34mII\x1b[0m] ${msg}\n`);
+                process.stdout.write(`\x1b[2K\r[\x1b[34mII\x1b[0m] ${msg}\n`);
             } else {
                 process.stdout.write(`[II] ${msg}\n`);
             }
             break;
         case "done":
             if (useColor) {
-                process.stdout.write(`\r${" ".repeat(blank)}\r[\x1b[32mOK\x1b[0m] ${msg}\n`);
+                process.stdout.write(`\x1b[2K\r[\x1b[32mOK\x1b[0m] ${msg}\n`);
             } else {
                 process.stdout.write(`[OK] ${msg}\n`);
             }
             break;
         case "step":
             if (useColor) {
-                process.stdout.write(`\r${" ".repeat(blank)}\r[\x1b[33m>>\x1b[0m] ${msg}`);
+                process.stdout.write(`\x1b[2K\r[\x1b[33m>>\x1b[0m] ${msg}`);
             } else {
                 process.stdout.write(`[>>] ${msg}\n`);
             }
